020f3c8e641170456f96ce5a56794432e5ccf4c2
Documenter.jl <documenter@juliadocs.github.io>
2023-12-22 21:52:21 +0000

    build based on 6274258

---
+++ v0.1.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MakieRichText","category":"page"},{"location":"#MakieRichText","page":"Home","title":"MakieRichText","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MakieRichText.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MakieRichText]","category":"page"}]
+}
